f95d860379e9b437cff17f53880987e7a23b63c9
root <root@perf36.perf.lab.eng.bos.redhat.com>
2015-11-11 08:13:54 -0500

    Some standard deviation error... debugging... this is a debug image not for general consumption

--- trafficgen.lua
+++ trafficgen.lua
@@ -68,8 +68,8 @@ function master(...)
 	while ( math.abs(rate - prevRate) > rate_resolution or final_validation_ctr < 1 ) do
 		-- r = {frame_loss, rxMpps, total_rx_frames, total_tx_frames}
 	        r = {dev1_frame_loss, dev1_rxMpps, dev1_total_x_frames, dev1_total_rx_frames, dev2_frame_loss, dev2_rxMpps, dev2_total_tx_frames, dev2_total_rx_frames, avg_device_frame_loss, aggregate_avg_rxMpps, dev1_frame_loss, dev2_frame_loss}
+                printf("TOP OF WHILE LOOP:  Testing with prevPassRate = %0.2f, prevFailRate = %.2f, prevRate = %.2f, rate = %.2f", prevPassRate, prevFailRate, prevRate, rate); 
 		launchTest(devs[1], devs[2], rate, bidirec, 0, frame_size, run_time, num_flows, method, r)
-		frame_loss = r[1]
 		local avg_device_frame_loss = r[9]
 		local aggregate_avg_rxMpps = r[10]
                 
@@ -82,12 +82,14 @@ function master(...)
                         printf("*********************************************************************************************************************************");
 			prevFailRate = rate
 			rate = ( prevPassRate + rate ) / 2
+                        printf("FAIL WHILE LOOP:  new rate = %.2f, prevFailRate = %.2f, prevRate = %.2f", rate, prevFailRate, prevRate); 
 		else --acceptable packet loss, increase rate
                         printf("*********************************************************************************************************************************");
 			printf("* Test Result:  PASSED - The traffic thoughput loss was %.8f %%, was did not exceed the maximum allowed loss (%.2f %%)", avg_device_frame_loss, max_acceptable_frame_loss);
                         printf("*********************************************************************************************************************************");
 			prevPassRate = rate
 			rate = (prevFailRate + rate ) / 2
+                        printf("PASS WHILE LOOP:  new rate = %.2f, prevPassRate = %.2f, prevRate = %.2f", rate, prevPassRate, prevRate); 
 		end
 		printf("\n")
 		dpdk.sleepMillis(500)
@@ -104,7 +106,8 @@ function master(...)
 	            printf("* Starting final validation");
                     printf("*********************************************************************************************");
                     printf("\n\n");
-	            launchTest(devs[1], devs[2], rate, bidirec, latency, frame_size, run_time, num_flows, method, r)
+                    printf("VALIDATION WHILE LOOP:  Testing with prevPassRate = %0.2f, prevFailRate = %.2f, prevRate = %.2f, rate = %.2f", prevPassRate, prevFailRate, prevRate, rate); 
+	            launchTest(devs[1], devs[2], prevRate, bidirec, latency, frame_size, run_time, num_flows, method, r)
                     printf("\n\n");
                     printf("*********************************************************************************************");
 	            printf("* Stopping final validation");
@@ -127,7 +130,6 @@ function master(...)
 	            local dev1_frame_loss = r[11]
 	            local dev2_frame_loss = r[12]
                     
-                    prevRate = rate
                     if (avg_device_frame_loss) > max_acceptable_frame_loss then
 
                         printf("\n");
@@ -139,8 +141,6 @@ function master(...)
                         printf("*********************************************************************************************");
                         printf("\n");
 
-			prevFailRate = rate
-			rate = ( prevPassRate + rate ) / 2
 
 	            else
                         printf("\n");
@@ -180,7 +180,7 @@ function master(...)
                         printf("    Average Rx Frame Count (Mfps) ........................... %.2f", dev2_rxMpps);
                         printf("    Rx Frame Count .......................................... %d", dev2_total_rx_frames);
                         printf("    Tx Frame Count .......................................... %d", dev2_total_tx_frames);
-                        printf("    Frame Loss .............................................. %d", dev2_frame_loss);
+                        printf("    Frame Loss .............................................. %d\n", dev2_frame_loss);
                         printf("#############################################################################################\n");
                         printf("\n\n");
                         final_validation_ctr = 1
