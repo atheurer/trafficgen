cd4c9334bd0ba3aa4952749d7da92af3af02328d
Karl Rister <krister@redhat.com>
2019-04-09 10:04:20 -0500

    launch-trex.sh: increase TRex server timeout
    - I'm getting much more consistent behavior with the timeout set to 60
      seconds instead of 30 seconds which ensures that tests run properly.

--- launch-trex.sh
+++ launch-trex.sh
@@ -225,7 +225,7 @@ if [ -d ${trex_dir} -a -d ${tmp_dir} ]; then
     screen -x trex -p server -X log on
 
     # wait for trex server to be ready                                                                                                                                                                                                    
-    count=30
+    count=60
     num_ports=0
     while [ ${count} -gt 0 -a ${num_ports} -lt 2 ]; do
         sleep 1
